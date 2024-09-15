bd74e0a73a48d9b7706a6962a9b9efbe9450c45f
TanaseTudor <163713702+TanaseTudor@users.noreply.github.com>
2024-09-15 22:03:32 +0300

    Add files via upload

---
+++ Tudor/baba1.js
@@ -0,0 +1,42 @@
+function secret1() {
+    let password = document.getElementById("w").value;
+
+    
+if(password == "RealTudor7qwertyuiop") {
+            
+    location.href = "secret2.html"
+            
+}
+}
+
+function secret() {
+    location.href = "secret.html"
+}
+
+function secret3() {
+    document.getElementById("q").innerHTML = "o plac pe alexandra"
+}
+
+function muzica() {
+    location.href = "muzica.html"
+}
+
+function prune() {
+    var audio = new Audio('prune.mp3');
+    audio.play();
+}
+
+function band4band() {
+    var audio = new Audio('band4band.mp3');
+    audio.play();
+}
+
+function marut() {
+    var audio = new Audio('marut.mp3');
+    audio.play();
+}
+
+function otomani() {
+    var audio = new Audio('sistemulotoman.mp3');
+    audio.play();
+}
